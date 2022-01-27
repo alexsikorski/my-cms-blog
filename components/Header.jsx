20982f447737ee557a2db55b915cb2eca9af648d
alexsikorski <alex@alexsikorski.net>
2022-01-27 11:50:09 +0000

    implemented recent posts/similar posts and categories elements

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link'
 
-const categories = [{ name: 'LeetCode Easy', slug: 'leetcode-easy' }, { name: 'Java Dev', slug: 'java-dev' }]
+
+import { getCategories } from '../services';
+
+
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, []);
+
     return <div className='container mx-auto px-10 mb-8'>
         <div className='border-b w-full inline-block border-white/20 py-8'>
             <div className='md:float-left block'>
                 <Link href='/'>
                     <span className='cursor-pointer font-bold text-4xl text-white'>
-                        Alex Sikorski<span className='text-black'>'s blog</span>
+                        Alex Sikorski<span className='text-amber-500'>'s blog</span>
                     </span>
                 </Link>
             </div>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -29,19 +29,19 @@ const PostCard = ({ post }) => {
                         className='align-middle rounded-full'
                         src={post.author.photo.url}
                     />
-                    <p className='inline align-middle text-zinc-900 ml-2 text-lg'>
+                    <p className='inline align-middle text-gray-600 ml-2 text-lg'>
                         {post.author.name}</p>
                 </div>
-                <div className='font-medium text-zinc-900'>
+                <div className='font-medium text-gray-600'>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-amber-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
                     <span>
-                        {moment(post.createdAt).format('DD MMM, YYYY')}
+                        {moment(post.createdAt).format('MMM DD, YYYY')}
                     </span>
                 </div>
             </div>
-            <p className='text-center text-lg text-zinc-900 font-normal px-4 lg:px-20 mb-8'>
+            <p className='text-center text-lg text-gray-600 font-normal px-4 lg:px-20 mb-8'>
                 {post.brief}
             </p>
             <div className='text-center'>
