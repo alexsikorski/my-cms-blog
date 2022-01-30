d571c83139cf5cc3dbce43c19eb344926118562c
alexsikorski <alex@alexsikorski.net>
2022-01-30 12:03:31 +0000

    updated tailwind and fixed carousel arrows

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -36,15 +36,15 @@ const FeaturedPosts = () => {
     }, []);
 
     function customLeftArrow() {
-        return <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+        return <div className='absolute left-0 cursor-pointer bg-amber-500 py-3 px-4 rounded-full text-center'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
         </div>
     }
 
-    function customRightArrow(){
-        return <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+    function customRightArrow() {
+        return <div className='absolute right-0 cursor-pointer bg-amber-500 py-3 px-4 rounded-full text-center'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
