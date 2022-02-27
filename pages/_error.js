7dd90bc8e15c403dbcf94fbf34d24e7618b24cdf
alexsikorski <alex@alexsikorski.net>
2022-02-27 14:02:09 +0000

    added _error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,15 +1,15 @@
 import { Categories, PostWidget } from '../components';
 import Link from 'next/link';
 
-export default function Custom404() {
-    return <>
+function Error({ statusCode }) {
+    return (
         <div className='container mx-auto px-10 mb-8'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
                 <div className='lg:col-span-8 col-span-1'>
                     <div className="col-span-1 lg:col-span-8 bg-white rounded-md p-8 text-center">
                         <div className='mb-8 '>
-                            <span className="text-center text-3xl font-semibold">404</span>
-                            <span className="text-center text-xl"> Page not found!</span>
+                            <span className="text-center text-3xl font-semibold">{statusCode}</span>
+                            <span className="text-center text-xl"> An error occured!</span>
                         </div>
                         <Link href='/'>
                             <a className="transition duration-500 transform hover:-translate-y-1 inline-block bg-amber-500 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
@@ -25,6 +25,12 @@ export default function Custom404() {
                 </div>
             </div>
         </div>
-
-    </>
+    )
 }
+
+Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
+}
+
+export default Error
