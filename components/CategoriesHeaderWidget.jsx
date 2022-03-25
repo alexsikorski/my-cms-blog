65f2471c00f4b630c353b481f7ea6ef592e317a1
alexsikorski <alex@alexsikorski.net>
2022-03-25 12:09:12 +0000

    implemented tailwind ui dropdown

--- components/CategoriesHeaderWidget.jsx
+++ components/CategoriesHeaderWidget.jsx
@@ -1,13 +1,99 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
 const categoriesHeaderWidget = () => {
-    return (<>
-                <button className='float-right ml-2 lg:ml-4 text-white lg:invisible md:visible sm:visible
-                 inline-block bg-amber-500 rounded-lg text-white cursor-pointer drop-shadow w-10 mt-2 text-center p-1'>
-            <svg xmlns="http://www.w3.org/2000/svg" className='fill-current h-4 w-4'
-                viewBox="0 0 20 20" stroke='white'>
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
-        </button>
-    </>)
+    // return (<>
+    //             <button className='float-right ml-2 lg:ml-4 text-white lg:invisible md:visible sm:visible
+    //              inline-block bg-amber-500 rounded-lg text-white cursor-pointer drop-shadow w-10 mt-2 text-center p-1'>
+
+    //     </button>
+    // </>)
+    return (
+        <Menu as="span" className="ml-2 md:ml-1 mt-2 float-right lg:float-left lg:invisible">
+            <div>
+                <Menu.Button className="inline-block justify-center w-full rounded-lg drop-shadow px-2 py-1 bg-amber-500 text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" className='fill-current h-4 w-4'
+                        viewBox="0 0 20 20" stroke='white'>
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Account settings
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Support
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    License
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <form method="POST" action="#">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        type="submit"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block w-full text-left px-4 py-2 text-sm'
+                                        )}
+                                    >
+                                        Sign out
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </form>
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
 
 }
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     }, []);
 
     return <div className='container mx-auto px-10 mb-4 select-none'>
-        <div className='border-b w-full inline-block border-white/5 py-6 '>
+        <div className='border-b w-full inline-block border-white/5 py-6'>
             <div className='md:float-left block
             transition duration-500 transform hover:-translate-y-1 '>
                 <Link href='/'>
@@ -51,7 +51,7 @@ const Header = () => {
                 <span className='hidden lg:float-left lg:contents'>
                     {categories.map((category) => (
                         <Link key={category.slug} href={`/category/${category.slug}`}>
-                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer
+                            <span className='md:float-right mt-2 align-middle text-white text-sm ml-4 font-semibold cursor-pointer
                          transition duration-500 transform hover:-translate-y-1 hover:text-amber-500'>
                                 {category.name}
                             </span>
