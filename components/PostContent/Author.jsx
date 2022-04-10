5034ee01836672ccfe60c336e2712810a0a7fd1f
alexsikorski <alex@alexsikorski.net>
2022-04-10 14:35:43 +0100

    redesign of author section

--- components/PostContent/Author.jsx
+++ components/PostContent/Author.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 
 const Author = ({author}) => {
-    return <div className='flex grid grid-cols-5 text-left mb-4 p-6 relative rounded-lg bg-white/[.045]'>
-        <div className='m-auto '>
+    return <div className='flex  items-center mb-4 p-8 rounded-lg bg-white/[.045]'>
+        <div>
             <img
                 alt={author.name}
                 height='100px'
                 width='100px'
-                className=' rounded-full shadow-lg'
+                className='rounded-full shadow-lg align-middle'
                 src={author.photo.url}
             />
         </div>
-        <div className='col-span-4 my-4 ml-4'>
-            <h3 className='text-white  text-xl font-bold'>{author.name}</h3>
-            <p className='text-white text-lg'>{author.bio}</p>
+        <div className="ml-4">
+            <h3 className='text-white text-xl font-bold'>{author.name}</h3>
+            <p className='text-white '>{author.bio}</p>
         </div>
-
     </div>;
 };
 
