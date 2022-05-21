64c2007aca15f29299591d316c5c0eae5a48ea3a
alexsikorski <alex@alexsikorski.net>
2022-05-21 10:33:58 +0100

    redesigned post card, added categories links

--- components/MainBar/PostCard.jsx
+++ components/MainBar/PostCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-const PostCard = ({post}) => {
+const PostCard = ({ post }) => {
     return (
         <div className='bg-lighter-washed-black shadow-lg rounded-lg mb-4 text-white'>
             <div className='relative overflow-hidden shadow-md pb-60'>
@@ -13,14 +13,34 @@ const PostCard = ({post}) => {
                 />
             </div>
             <div className='lg:p-10 md:p-10 p-4'>
-                <h1 className='inline-block transition duration-400 text-left mb-4 cursor:pointer
+                <h1 className='inline-block transition duration-400 text-left
                  hover:text-amber-500 text-3xl font-semibold'>
                     <Link href={`/post/${post.slug}`}>
                         {post.title}
                     </Link>
 
                 </h1>
-                <p className='text-left text-lg text-neutral-200 font-medium  mb-4'>
+                <div className='flex justify-center items-center'>
+                    <div className='flex-auto'>
+                        {post.categories.map((category) => {
+                            return (
+                                <Link href={`/category/${category.slug}`} key={category.slug}>
+                                    <p className='inline mr-2 text-sm font-medium text-neutral-400 transition duration-400 cursor-pointer hover:text-amber-500'>
+                                        #{category.name}
+                                    </p>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                    <div className='flex-auto'>
+                        <p className='text-right font-medium text-sm text-neutral-400'>
+                            {moment(post.createdAt).format('MMM DD, YYYY')}
+                        </p>
+                    </div>
+                </div>
+
+
+                <p className='text-left text-lg text-neutral-200 font-medium  mt-4 mb-4'>
                     {post.brief}
                 </p>
                 <div className='mt-4 lg:mb-0 w-full h-full lg:w-auto flex'>
@@ -29,24 +49,21 @@ const PostCard = ({post}) => {
                             alt={post.author.name}
                             height='30px'
                             width='30px'
-                            className='inline align-middle rounded-full'
+                            className='inline rounded-full align-middle'
                             src={post.author.photo.url}
                         />
-                        <p className='inline font-normal align-middle text-neutral-400 ml-2 '>
+                        <p className='ml-2 inline font-normal text-lg text-neutral-400 align-middle'>
                             {post.author.name}</p>
-                        <span className='ml-2 font-normal align-middle text-xs text-neutral-400'>
 
-                            {moment(post.createdAt).format('MMM DD, YYYY')}
-                        </span>
                     </span>
-                    <span className='align-middle'>
+                    <span>
                         <Link href={`/post/${post.slug}`}>
                             <div className='p-1 transition duration-400 transform hover:-translate-x-1 bg-amber-500
                                 rounded-lg shadow-lg text-white cursor-pointer w-16'>
                                 <svg xmlns="http://www.w3.org/2000/svg" height='22' width='16' className='m-auto'
-                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"
-                                          d="M14 5l7 7m0 0l-7 7m7-7H3"/>
+                                        d="M14 5l7 7m0 0l-7 7m7-7H3" />
                                 </svg>
                             </div>
 
